041bb8693f8d1f001612354dd24fb8242fcf8791
yuma-matsui <oshino.iiyo@gmail.com>
2020-09-08 16:45:41 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+      
+  validates :nickname, presence: true
+  validates :email, uniqueness: true, format: { with: /.+@.+/ }
+  validates :password, presence: true, confirmation: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i }
+            
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+            
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン]+\z/ } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+            
+  validates :birth_date, presence: true
+       
 end
