10e93b789f194440420f0f3ddcbd510ba30db662
yuma-matsui <oshino.iiyo@gmail.com>
2020-09-09 11:52:47 +0900

    UsersテーブルとItemsテーブルのアソシエーション設定

---
+++ app/models/item.rb
@@ -0,0 +1,14 @@
+class Item < ApplicationRecord
+
+  belongs_to :user
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :delivery_when
+  belongs_to_active_hash :delivery_where
+
+  validates :name, :content, :price, :category, :status, :delivery_fee, :delivery_when, :delivery_where, presence: true
+  validates :category_id, :status_id, :delivery_fee_id, :delivery_when_id, :delivery_where_id, numericality: { other_than: 1 }
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items      
+
   validates :nickname, presence: true
   validates :email, uniqueness: true, format: { with: /.+@.+/ }
   validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i }
