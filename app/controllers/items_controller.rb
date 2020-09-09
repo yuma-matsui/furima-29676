3d1242a6b424912632271d965173fc32bfe10488
yuma-matsui <oshino.iiyo@gmail.com>
2020-09-09 12:27:41 +0900

    出品機能のためのコントローラー、ビューファイル編集(画像投稿機能を除く)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,16 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
-  
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :content, :price, :category_id, :status_id, :delivery_fee_id, :delivery_when_id, :delivery_where_id).merge(user_id: current_user.id)
+  end
+
 end
