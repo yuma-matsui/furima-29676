192d3f795fb1f32f8f1abb5ba3e9049d5cd7bc9d
yuma-matsui <oshino.iiyo@gmail.com>
2020-09-10 21:15:07 +0900

    コントローラー内にdestroyアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :find_params, only: [:show, :edit, :update]
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order(created_at: "DESC")
@@ -25,6 +25,11 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
