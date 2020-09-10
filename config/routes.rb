d700f6018dc145d67515e0fe11e294ef6b3b99fa
yuma-matsui <oshino.iiyo@gmail.com>
2020-09-10 14:38:00 +0900

    詳細ページへのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'items/index'
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create, :show]
 end
