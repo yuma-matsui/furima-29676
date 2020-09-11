154a04dfa953979500effca68f967b0808a17067
yuma-matsui <oshino.iiyo@gmail.com>
2020-09-11 15:22:39 +0900

    Ordersテーブルの作成とアソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items      
+  has_many :items 
+  has_many :orders     
 
   validates :nickname, presence: true
   validates :email, uniqueness: true, format: { with: /.+@.+/ }
