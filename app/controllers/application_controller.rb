267965403a3081a481383e456848755bb25e6b5e
yuma-matsui <oshino.iiyo@gmail.com>
2020-09-11 17:35:34 +0900

    出品者、非サインアップユーザーのリダイレクト設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[nickname first_name last_name first_name_kana last_name_kana birth_date])
-  end
+  end   
+  
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,25 @@
 class OrdersController < ApplicationController
+  before_action :move_to_session
+  before_action :move_to_index
 
   def index
     @order = Order.new
     @item = Item.find(params[:item_id])
   end
+  
+  private
+  def move_to_session
+    unless user_signed_in? 
+      redirect_to user_session_path
+    end
+  end
+  
+  def move_to_index
+    item = Item.find(params[:item_id])
+    if current_user.id == item.user_id
+      redirect_to root_path
+    end
+  end
+
 
 end
