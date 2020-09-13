03e19f10c69e1e7640b1aa0af0556c6312207133
yuma-matsui <oshino.iiyo@gmail.com>
2020-09-13 11:34:10 +0900

    購入時の入力内容をOrdersテーブルとAddressesテーブルへの一括保存実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,14 @@
 class OrdersController < ApplicationController
-  before_action :move_to_session
-  before_action :move_to_index
+  before_action :move_to_session, only: [:index]
+  before_action :move_to_index, only: [:index]
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @order = Order.new
-    @item = Item.find(params[:item_id])
+    @order = OrderAddress.new
   end
-  @address = Address.new
 
   def create
-    @order = Order.new(credit_params)
-    @item = Item.find(params[:item_id])
+    @order = OrderAddress.new(credit_params)
     if @order.valid?
       pay_item
       @order.save
@@ -19,16 +17,6 @@ class OrdersController < ApplicationController
       render :index
     end
   end
-    # @order = OrderAddress.new(address_params)
-    # @order.save
-    # if @order.valid?
-    #   pay_item
-    #   @order.save
-    #   return redirect_to root_path
-    # else
-    #   render :index
-    # end
-  
   
   private
   def move_to_session
@@ -39,15 +27,20 @@ class OrdersController < ApplicationController
   
   def move_to_index
     item = Item.find(params[:item_id])
-    if current_user.id == item.user_id
+    order = Order.new
+    if current_user.id == item.user_id || item.order != order.item_id
       redirect_to root_path
     end
   end
 
   def credit_params
-    params.permit(:token, :item_id).merge(user_id: current_user.id)
+    params.permit(:post_number, :prefecture_id, :city_name, :house_number, :building_name, :phone_number, :token, :item_id).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+    
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
@@ -58,13 +51,5 @@ class OrdersController < ApplicationController
   end
 
 end
-  # def address_params
-  #   params.require(:order_address).permit(:post_number, :prefecture_id, :city_name, :house_number, :building_number, :phone_number)
-  # end
-  # def order_params
-  #   params.permit(:token)
-  # end
 
-  
-
-
+ 
---
+++ app/forms/order_address.rb
@@ -0,0 +1,19 @@
+class OrderAddress
+
+  include ActiveModel::Model
+  attr_accessor :token, :post_number, :prefecture_id, :city_name, :house_number, :building_name, :phone_number, :item_id, :user_id
+
+  with_options presence: true do
+    validates :post_number, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :city_name
+    validates :house_number
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+  end
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :token, presence: true
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(post_number: post_number, prefecture_id: prefecture_id, city_name: city_name, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
+end
