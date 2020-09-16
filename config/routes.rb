838dff134bf9bda1b482613d3c2b2f95177bcb3e
yuma-matsui <oshino.iiyo@gmail.com>
2020-09-16 17:02:41 +0900

    ユーザー詳細ページ、情報更新のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
   get 'items/index'
   devise_for :users
   root to: 'items#index'
   resources :items do
     resources :orders, only: %i[index create]
   end
+  resources :users, only: [:show, :update]
 end
