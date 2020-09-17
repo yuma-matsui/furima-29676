a9ee00f9d40a33bbf866ba1b68932e6aa12b273a
yuma-matsui <oshino.iiyo@gmail.com>
2020-09-17 16:32:24 +0900

    ユーザー情報編集機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
-  def show
+  before_action :user_find, only: [:edit, :update]
+
+
+  def update
+    if @user.update(user_params)
+      sign_in(@user, bypass: true)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date, :password)
+  end
+
+  def user_find
+    @user = User.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :items do
     resources :orders, only: %i[index create]
   end
-  resources :users, only: [:show, :update]
+  resources :users, only: [:edit, :update]
 end
