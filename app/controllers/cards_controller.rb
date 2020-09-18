feb3e262f71d3a76e4855758032b54ff82609d0a
yuma-matsui <oshino.iiyo@gmail.com>
2020-09-18 19:28:16 +0900

    show / destroyアクションの設定

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,21 @@
 class CardsController < ApplicationController
+
   def new
+    card = Card.where(user_id: current_user.id).first
+    unless card.blank?
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def show
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    @credit = Card.find_by(user_id: current_user.id)
+    if @credit.blank?
+      redirect_to action: "new"
+    else
+      customer = Payjp::Customer.retrieve(@credit.customer_token) 
+      @card = customer.cards.first
+    end
   end
 
   def create
@@ -20,8 +36,18 @@ class CardsController < ApplicationController
       card.save
       redirect_to root_path
     end
-
   end
 
+  def destroy
+    card = Card.where(user_id: current_user.id).first
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    customer = Payjp::Customer.retrieve(card.customer_token) 
+    if customer.delete
+       card.delete
+       redirect_to root_path
+    else
+      redirect_to action: "show"
+    end
+  end
 
 end
