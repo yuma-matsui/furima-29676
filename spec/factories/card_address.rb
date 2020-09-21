b4d174b6ebe60d3af156ab622e21fa3e2eb81fbc
yuma-matsui <oshino.iiyo@gmail.com>
2020-09-21 15:57:45 +0900

    CardAddressモデルの単体テストコード実施

---
+++ spec/factories/card_address.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :card_address do
+    post_number { '111-1111' }
+    prefecture_id { 2 }
+    city_name { '東京'}
+    house_number { '世田谷区' }
+    building_name { 'セタガヤハイツ' }
+    phone_number { '00000000000' }
+  end
+end
