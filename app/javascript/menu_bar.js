acff5b256b10ebbfce6a1637e7ad5477d2579ced
yuma-matsui <oshino.iiyo@gmail.com>
2020-09-23 17:33:07 +0900

    マイページのアニメーション設定

--- app/javascript/menu_bar.js
+++ app/javascript/menu_bar.js
@@ -5,9 +5,9 @@ function setMenu () {
 
   userMenu.addEventListener('click', function () {
      if (menuLists.getAttribute("class") == "lists-right-none") {
-      menuLists.removeAttribute("class", "lists-right-none")
+      menuLists.removeAttribute("class", "lists-right-none");
      } else {
-       menuLists.setAttribute("class", "lists-right-none")
+       menuLists.setAttribute("class", "lists-right-none");
      };
   });
 };
